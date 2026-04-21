d87685a37f66ee14a89bb6e3665a57ff285a9401
Adrian <moldovan.adrian.alexandru03@gmail.com>
2026-04-21 12:01:32 +0300

    Created the components folder and about component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from "./components/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
