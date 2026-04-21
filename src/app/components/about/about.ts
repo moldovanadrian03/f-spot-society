792bfded640e67201efbe5c1fe9b8721715ea89b
Adrian <moldovan.adrian.alexandru03@gmail.com>
2026-04-21 12:24:09 +0300

    Created an utils folder, created openLink function and other minor changes to about component.

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CONTRACT_PERSOANE_JURIDICE, FORMULAR_PERSOANE_FIZICE, openLink } from '../../utils';
 
 @Component({
   selector: 'app-about',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
-export class About {}
+export class About {
+
+  openLink (url: string): void {
+    openLink(url);
+  }
+
+  FORMULAR_PERSOANE_FIZICE = FORMULAR_PERSOANE_FIZICE;
+  CONTRACT_PERSOANE_JURIDICE = CONTRACT_PERSOANE_JURIDICE;
+}
---
+++ src/app/utils.ts
@@ -0,0 +1,6 @@
+export function openLink(url: string): void {
+  window.open(url, '_blank');
+}
+
+export const FORMULAR_PERSOANE_FIZICE = 'https://formular230.ro/asociatia-culturala-f-spot-society';
+export const CONTRACT_PERSOANE_JURIDICE = 'https://sites.google.com/view/fspotsociety/FSpot_companii?authuser=0';
