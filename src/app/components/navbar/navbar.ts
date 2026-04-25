4611f9d5c0a40aa784c280a894243e4d59b85686
Adrian <moldovan.adrian.alexandru03@gmail.com>
2026-04-25 10:27:36 +0300

    Added the progress bar functionality to the navbar component.

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,19 +1,23 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { ABOUT_SECTION, MEMBERS_SECTION, FOOTER_SECTION, INFO_SECTION, scrollToElementById, PROJECTS_SECTION } from '../../utils';
+import { After } from 'v8';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    MatIconModule
+    MatIconModule,
+    NgIf
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar implements OnInit {
+export class Navbar implements OnInit, AfterViewInit {
   isSidenavOpened: boolean = false;
   isMobileView: boolean = false;
   resizeObserver: ResizeObserver | null = null;
+  activeSection: string = '';
 
   ngOnInit(): void {
     this.updateIsMobileView();
@@ -21,6 +25,10 @@ export class Navbar implements OnInit {
       this.updateIsMobileView();
     });
     this.resizeObserver.observe(document.body);
+  }
+
+  ngAfterViewInit(): void {
+      this.onScroll();
   }
 
   @HostListener('window:resize', ['$event'])
@@ -39,6 +47,44 @@ export class Navbar implements OnInit {
 
   scrollTo(element: string): void {
     scrollToElementById(element);
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const sections = [
+      this.ABOUT_SECTION,
+      this.INFO_SECTION,
+      this.MEMBERS_SECTION,
+      this.PROJECTS_SECTION,
+      this.FOOTER_SECTION
+    ];
+
+    const scrollPosition = window.innerHeight + window.scrollY;
+    const pageHeight = document.documentElement.scrollHeight;
+
+    //detectare footer
+    if (scrollPosition >= pageHeight - 50) {
+      if (this.activeSection !== this.FOOTER_SECTION) {
+        this.activeSection = this.FOOTER_SECTION;
+      }
+      return;
+    }
+
+    const offset = window.innerHeight * 0.2;
+
+    for (let id of sections) {
+      const el = document.getElementById(id);
+      if (!el) continue;
+
+      const rect = el.getBoundingClientRect();
+
+      if (rect.top <= offset && rect.bottom >= offset) {
+        if (this.activeSection !== id) {
+          this.activeSection = id;
+        }
+        break;
+      }
+    }
   }
 
   ABOUT_SECTION = ABOUT_SECTION;
